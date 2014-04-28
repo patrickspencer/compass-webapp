84f966f3e5083bec399a22253b943999ce7e6728
Patrick Spencer <patrick.spencer@mizzou.edu>
2014-04-28 09:07:31 -0500

    Updated Group and GroupUser models. Need to test them in next commit.

--- db/migrate/20140417162854_create_assignments.rb
+++ db/migrate/20140417162854_create_assignments.rb
@@ -2,7 +2,7 @@ class CreateAssignments < ActiveRecord::Migration
   def change
     create_table :assignments do |t|
       t.string :name
-      t.timestamps
+      t.belongs_to :assignment_type
     end
   end
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save course without title" do
+    course = Course.new
+    assert_not course.save
+  end
 end
